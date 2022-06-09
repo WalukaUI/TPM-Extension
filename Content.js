b744db78ba9696f326d0a52087aad94a34912279
WalukaUI <cwaluka@yahoo.com>
2022-06-08 21:29:56 -0500

    tag changed

--- Content.js
+++ Content.js
@@ -1,6 +1,6 @@
 let newData = "Lukas Extension";
 
-const pTag = document.getElementsByTagName("p");
+const pTag = document.getElementsByTagName("a");
 for (let i = 0; i < pTag.length; i++) {
   pTag[i].innerHTML = newData;
 }
